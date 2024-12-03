4794ea18d9d39bf59691b44d2a20c8ef2fcdfe52
Felix Salim <felixsalim64@gmail.com>
2024-12-03 22:02:00 +0700

    feat: added player animation

--- Player/States/RunState.gd
+++ Player/States/RunState.gd
@@ -4,16 +4,20 @@ extends State
 # RunState class
 class_name RunState
 
-# Get Input, Detects when player stop moving, if so, change state to idle
+# Get Input, Detects when player stop moving, if so, change state to idle and set animation to idle
 func get_input():
 	if Input.is_action_just_released("ui_left"):
 		changeState.call("idle")
+		playerAnimation.play("Idle Left")
 	if Input.is_action_just_released("ui_right"):
 		changeState.call("idle")
+		playerAnimation.play("Idle Right")
 	if Input.is_action_just_released("ui_up"):
 		changeState.call("idle")
+		playerAnimation.play("Idle Up")
 	if Input.is_action_just_released("ui_down"):
 		changeState.call("idle")
+		playerAnimation.play("Idle Down")
 
 # Run get_input every fixed interval
 func _physics_process(delta):
@@ -23,23 +27,23 @@ func _physics_process(delta):
 func move_left():
 	player.velocity.x -= Input.get_action_strength("ui_left")
 	if Input.get_action_strength("ui_left") > Input.get_action_strength("ui_up") and Input.get_action_strength("ui_left") > Input.get_action_strength("ui_down"):
-		playerAnimation.play("Idle Left")
+		playerAnimation.play("Walk Left")
 		hitboxAnimation.play("Interact Left")
 
 func move_right():
 	player.velocity.x += Input.get_action_strength("ui_right")
 	if Input.get_action_strength("ui_right") > Input.get_action_strength("ui_up") and Input.get_action_strength("ui_right") > Input.get_action_strength("ui_down"):
-		playerAnimation.play("Idle Right")
+		playerAnimation.play("Walk Right")
 		hitboxAnimation.play("Interact Right")
 
 func move_up():
 	player.velocity.y -= Input.get_action_strength("ui_up")
 	if Input.get_action_strength("ui_up") > Input.get_action_strength("ui_left") and Input.get_action_strength("ui_up") > Input.get_action_strength("ui_right"):
-		playerAnimation.play("Idle Up")
+		playerAnimation.play("Walk Up")
 		hitboxAnimation.play("Interact Up")
 
 func move_down():
 	player.velocity.y += Input.get_action_strength("ui_down")
 	if Input.get_action_strength("ui_down") > Input.get_action_strength("ui_left") and Input.get_action_strength("ui_down") > Input.get_action_strength("ui_right"):
-		playerAnimation.play("Idle Down")
+		playerAnimation.play("Walk Down")
 		hitboxAnimation.play("Interact Down")
