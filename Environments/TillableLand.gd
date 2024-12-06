c41c019ee38a3711f281b7ed39a3a36d2791ce83
Felix Salim <felixsalim64@gmail.com>
2024-12-06 16:05:35 +0700

    feat: added commands

--- Environments/TillableLand.gd
+++ Environments/TillableLand.gd
@@ -6,23 +6,33 @@ class_name TillableLand
 # Gets player state onready
 @onready var interactingState = get_parent().get_node("Player/interactingState")
 
+# Stores this current tile state
 var tileState = "Tillable"
+
+# Stores any object colliding this tile
 var collision = []
 
+# Called every 1/60s (1 frame) 
+# Set the text of this state (prototype, later will change the sprite) to the state it is on
+# Enters the state machine so that player can interact with this tile
 func _process(delta):
 	get_node("Label").text = tileState
 	if collision.size() > 0:
 		enter()
 
+# Updates the tilestate
 func change_tile_state(tileState):
 	self.tileState = tileState
 
+# Calls the corresponding state machine so that player can interact
 func enter():
 	if Game.currentInteractingState != "None" and interactingState.get_node(Game.currentInteractingState) != null:
 		interactingState.get_node(Game.currentInteractingState).interact(self, tileState, collision)
 
+# When a collision occurs, add the collision to the array
 func _on_area_entered(area: Area2D) -> void:
 	collision.append(area)
 
+# When a collision no longer exists, remove the collision from the array
 func _on_area_exited(area: Area2D) -> void:
 	collision.erase(area)
--- Player/States/TillState.gd
+++ Player/States/TillState.gd
@@ -1,7 +1,9 @@
 extends InteractingState
 
+# Declares a class
 class_name TillState
 
+# When player interact with the land, tile the land
 func interact(tile, tileState, collision):
 	if tileState == "Tillable":
 		if Input.is_action_pressed("ui_accept"):
