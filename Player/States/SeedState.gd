1f0d0e29de8bdc468b31797b7eaaca6b05e48950
KevinBrave98 <kevinbravewq@gmail.com>
2024-12-11 00:08:50 +0700

    added and implemented basic tools animation

--- Player/States/SeedState.gd
+++ Player/States/SeedState.gd
@@ -7,4 +7,12 @@ class_name SeedState
 func interact(tile, tileState, collision):
 	if player.get_node("InteractBox") in collision and tileState == "Watered":
 		if Input.is_action_pressed("ui_accept"):
+			if player.lastDir == "left":
+				playerAnimation.play("SeedLeft")
+			if player.lastDir == "right":
+				playerAnimation.play("SeedRight")
+			if player.lastDir == "down":
+				playerAnimation.play("SeedDown")
+			if player.lastDir == "up":
+				playerAnimation.play("SeedUp")
 			tile.tileState = "Planted"
--- Player/States/WaterState.gd
+++ Player/States/WaterState.gd
@@ -7,4 +7,13 @@ class_name WaterState
 func interact(tile, tileState, collision):
 	if player.get_node("InteractBox") in collision and tileState == "Tilled":
 		if Input.is_action_pressed("ui_accept"):
+			if player.lastDir == "left":
+				playerAnimation.play("WaterLeft")
+			if player.lastDir == "right":
+				playerAnimation.play("WaterRight")
+			if player.lastDir == "down":
+				playerAnimation.play("WaterDown")
+			if player.lastDir == "up":
+				playerAnimation.play("WaterUp")
+				
 			tile.tileState = "Watered"
