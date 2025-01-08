5240793b9df06df1ef0dd34e277325b025e16c90
KevinBrave98 <kevinbravewq@gmail.com>
2025-01-08 13:19:26 +0700

    added npc collision and interaction

--- NPC/Red Panda/RedPanda.gd
+++ NPC/Red Panda/RedPanda.gd
@@ -1,11 +1,27 @@
-extends Node
+extends Area2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+# Stores colliding objects
+var collision = []
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+# If player interaction is colliding, show notification
 func _process(delta: float) -> void:
-	pass
+	if collision.size() > 0:
+		enter()
+
+func enter():
+	# Stores player
+	var player = get_parent().get_parent().get_node("Player/Player")
+	# If player is colliding and pressed interact, pop notification
+	if player.get_node("InteractBox") in collision and Input.is_action_just_pressed("ui_accept") and player.movementState.name != "notify":
+		player.change_moving_state("notify")
+		player.get_node("Control/UI/Notification").set_notification("Hi, I am Lign, I am the guardian of this land")
+
+# When a collision occurs, add the collision to the array
+func _on_area_entered(area: Area2D) -> void:
+	collision.append(area)
+
+# When a collision no longer exists, remove the collision from the array
+func _on_area_exited(area: Area2D) -> void:
+	collision.erase(area)
