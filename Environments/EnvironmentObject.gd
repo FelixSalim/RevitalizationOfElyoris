92a84f781e9d0b37fe72c39b738f7c119c2f8de5
KevinBrave98 <kevinbravewq@gmail.com>
2025-01-11 19:36:22 +0700

    added environment object class for opacity, added mailbox opacity change, change tree and mailbox parent

---
+++ Environments/EnvironmentObject.gd
@@ -0,0 +1,22 @@
+class_name EnvironmentObject
+
+extends Area2D
+
+var collision = []
+
+func change_opacity(player, collision):
+	if player in collision:
+		while get_node("../Sprite2D").modulate.a > 0.3:
+			print("a");
+			get_node("../Sprite2D").modulate.a -= 0.1
+			await get_tree().create_timer(0.1).timeout
+	else:
+		while get_node("../Sprite2D").modulate.a < 1:
+			get_node("../Sprite2D").modulate.a += 0.1
+			await get_tree().create_timer(0.1).timeout
+
+func _on_body_entered(area: Node2D) -> void:
+	collision.append(area)
+
+func _on_body_exited(area: Node2D) -> void:
+	collision.erase(area)
--- Environments/Mailbox.gd
+++ Environments/Mailbox.gd
@@ -1,9 +1,6 @@
-extends Area2D
+extends EnvironmentObject
 
 # Stores colliding objects
-var collision = []
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 # If player interaction is colliding, show notification
 func _process(delta: float) -> void:
@@ -13,15 +10,17 @@ func _process(delta: float) -> void:
 func enter():
 	# Stores player
 	var player = get_node("../../Player/Player")
+	change_opacity(player, collision)
 	# If player is colliding and pressed interact, pop notification
 	if player.get_node("InteractBox") in collision and Input.is_action_just_pressed("ui_accept") and player.movementState.name != "notify":
 		player.change_moving_state("notify")
 		player.get_node("Control/UI/Notification").set_notification("No mail for today")
 
+	
 # When a collision occurs, add the collision to the array
 func _on_area_entered(area: Area2D) -> void:
 	collision.append(area)
-
+	
 # When a collision no longer exists, remove the collision from the array
 func _on_area_exited(area: Area2D) -> void:
 	collision.erase(area)
