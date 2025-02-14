2353fc5fa6e1ca288a58b6081be0ecc1ca38f556
Felix Salim <felixsalim64@gmail.com>
2025-02-14 16:27:55 +0700

    fix : fixing some code tidyness

--- Player/QuestManager.gd
+++ Player/QuestManager.gd
@@ -12,24 +12,28 @@ func _ready() -> void:
 		get_node("Panel/QuestRequirements").add_child(control)
 		control.position = Vector2(0, 75 * i)
 		
-		var questDescription = Label.new()
-		questDescription.name = "QuestDesc"
-		questDescription.add_theme_font_size_override("font_size", 250)
-		questDescription.add_theme_color_override("font_color", Color.BLACK)
-		questDescription.scale = Vector2(0.2, 0.2)
-		questDescription.position = Vector2(2, 101)
-		questDescription.text = curQuest["Requirements"][i]["QuestDesc"]
-		control.add_child(questDescription)
+		update_quest_description(i, curQuest, control)
+		update_quest_progress(i, curQuest, control)
 		
-		var questProgress = Label.new()
-		questProgress.name = "Progress"
-		questProgress.add_theme_font_size_override("font_size", 250)
-		questProgress.add_theme_color_override("font_color", Color.BLACK)
-		questProgress.scale = Vector2(0.2, 0.2)
-		questProgress.position = Vector2(1324, 101)
-		questProgress.size = Vector2(2129, 342)
-		questProgress.horizontal_alignment = HORIZONTAL_ALIGNMENT_RIGHT
-		questProgress.text =  "<" + str(curQuest["Requirements"][i]["CurrentProgress"]) + "/" + str(curQuest["Requirements"][i]["MaxProgress"]) + ">"
-		control.add_child(questProgress)
-		
-		
+
+func update_quest_description(i, curQuest, control):
+	var questDescription = Label.new()
+	questDescription.name = "QuestDesc"
+	questDescription.add_theme_font_size_override("font_size", 250)
+	questDescription.add_theme_color_override("font_color", Color.BLACK)
+	questDescription.scale = Vector2(0.2, 0.2)
+	questDescription.position = Vector2(2, 101)
+	questDescription.text = curQuest["Requirements"][i]["QuestDesc"]
+	control.add_child(questDescription)
+
+func update_quest_progress(i, curQuest, control):
+	var questProgress = Label.new()
+	questProgress.name = "Progress"
+	questProgress.add_theme_font_size_override("font_size", 250)
+	questProgress.add_theme_color_override("font_color", Color.BLACK)
+	questProgress.scale = Vector2(0.2, 0.2)
+	questProgress.position = Vector2(1324, 101)
+	questProgress.size = Vector2(2129, 342)
+	questProgress.horizontal_alignment = HORIZONTAL_ALIGNMENT_RIGHT
+	questProgress.text =  "<" + str(curQuest["Requirements"][i]["CurrentProgress"]) + "/" + str(curQuest["Requirements"][i]["MaxProgress"]) + ">"
+	control.add_child(questProgress)
