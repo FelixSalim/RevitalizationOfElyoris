668d86b2ef79ceedc4b33eb4a245be65a884d8b6
Felix Salim <felixsalim64@gmail.com>
2025-03-12 13:11:00 +0700

    feat : updated main menu buttons

--- MainMenu.gd
+++ MainMenu.gd
@@ -4,6 +4,20 @@ extends Node2D
 func _on_touch_screen_button_pressed():
 	# Navigate to "World" Scene
 	get_tree().change_scene_to_file("res://Areas/World.tscn")
+	Utils.save_game()
 	
 	# Load save
 	Utils.load_game()
+
+
+func _on_touch_screen_button_2_pressed() -> void:
+	# Navigate to "World" Scene
+	get_tree().change_scene_to_file("res://Areas/World.tscn")
+	
+	# Load save
+	Utils.load_game()
+
+
+func _on_touch_screen_button_3_pressed() -> void:
+	# Exit game
+	get_tree().quit()
